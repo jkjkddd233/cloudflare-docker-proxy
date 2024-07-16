4d34f5088ebf237c1444b8767d8d433e9dc7bd3e
jkjkddd233 <119794576+jkjkddd233@users.noreply.github.com>
2024-07-16 08:49:25 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,14 +7,14 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
-  "ecr.libcuda.so": "https://public.ecr.aws",
+  "docker.suki0d00.us.kg": dockerHub,
+  "quay.suki0d00.us.kg": "https://quay.io",
+  "gcr.suki0d00.us.kg": "https://gcr.io",
+  "k8s-gcr.suki0d00.us.kg": "https://k8s.gcr.io",
+  "k8s.suki0d00.us.kg": "https://registry.k8s.io",
+  "ghcr.suki0d00.us.kg": "https://ghcr.io",
+  "cloudsmith.suki0d00.us.kg": "https://docker.cloudsmith.io",
+  "ecr.suki0d00.us.kg": "https://public.ecr.aws",
 
   // staging
   "docker-staging.libcuda.so": dockerHub,
